0d48ebf088c514c645f264eb853a2cab696c7332
Rauf Erk <erk.rauf@gmail.com>
2019-03-21 15:54:00 +0300

    all files is deleted

--- src/client/utils/bem.js
+++
@@ -1,7 +0,0 @@
-export function bemClassNameFactory(block) {
-  return (element, modificator) => {
-    if (!element) return block;
-    const baseName = `${block}__${element}`;
-    return modificator ? `${baseName} ${baseName}_${modificator}` : baseName;
-  };
-}
